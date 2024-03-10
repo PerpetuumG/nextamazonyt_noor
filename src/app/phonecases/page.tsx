2dc463669959925ed3e4badedcd7d6643786976c
PerpetuumG <perpetuumg@gmail.com>
2024-03-10 12:53:51 +0200

    Phones, Phonecases, Watches, Accessories

--- src/app/phonecases/page.tsx
+++ src/app/phonecases/page.tsx
@@ -1,20 +1,20 @@
-import { getPhones } from '@/helpers';
+import { getPhoneCases } from '@/helpers';
 import Container from '@/components/Container';
-import Product from "@/components/Product";
+import Product from '@/components/Product';
 
 const Page = async () => {
-  const products = await getPhones();
+  const products = await getPhoneCases();
 
   return (
     <Container>
       <div className={'border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between'}>
-        <h2>Phones</h2>
-        <p>Get the Phone you want </p>
+        <h2>Phone Cases</h2>
+        <p>Get the Phonecases you want </p>
       </div>
 
       <p className={'mt-4 text-zinc-500 font-semibold'}>Showing all {products.length} results </p>
 
-        <Product products={products}/>
+      <Product products={products} />
     </Container>
   );
 };
--- src/app/watches/page.tsx
+++ src/app/watches/page.tsx
@@ -1,20 +1,20 @@
-import { getPhones } from '@/helpers';
+import { getWatches } from '@/helpers';
 import Container from '@/components/Container';
-import Product from "@/components/Product";
+import Product from '@/components/Product';
 
 const Page = async () => {
-  const products = await getPhones();
+  const products = await getWatches();
 
   return (
     <Container>
       <div className={'border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between'}>
-        <h2>Phones</h2>
-        <p>Get the Phone you want </p>
+        <h2>Watches</h2>
+        <p>Get the Watch you want </p>
       </div>
 
       <p className={'mt-4 text-zinc-500 font-semibold'}>Showing all {products.length} results </p>
 
-        <Product products={products}/>
+      <Product products={products} />
     </Container>
   );
 };
